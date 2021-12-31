97015b8397f63418ed7faed529da004f2097d1e4
Afrah Ali <aali179@uottawa.ca>
2021-12-31 01:22:46 -0500

    add artist

--- spotify.py
+++ spotify.py
@@ -4,7 +4,7 @@ from spotipy.oauth2 import SpotifyClientCredentials
 cid = '5410e774c83040bb85936efc4672828a'
 secret = 'e893db8c750a439489d6a8c11560e470'
 client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
-token = 'BQBM5OrZgJUgas4LosYfq_kH6JxDe9q8TofjhPoA1T6KY_WA9wIIkEumg-8sT4kdpGvaPj1pELBFklY40qRci7Z99tp8x_ACkF5sogjTMb-ny6XwXqr48xmG9JX7Ei5TtJoBiRDkFAumWkQUAiWAPomYEoJAtME'
+token = 'BQD_t96THKx75LImnD5rUuBZA-d1Kwgk-R7ERzH4hjBkP8iXHUdYRu7AtU4SJ835pv94PjfFDloE3GIq19ImP9keuxYRBkVS9bXS_yyshNQGYyR69N-Ea5Tcv_LY96mj5HAuGAqyAmKbM-nPOFP0h9Dg42iEPN0'
 spotify = spotipy.Spotify(token)
 
 def get_track_name(trackId):
@@ -13,6 +13,9 @@ def get_track_name(trackId):
 def get_acoustic_feature(trackId, featureName):
     return spotify.audio_features(trackId)[0][featureName]
 
+def get_track_artist(trackId):
+    return spotify.track(trackId)['album']['artists'][0]['name']
+
 # labelled dataset
 dataset = [{"genre": "eerie", "trackId" : "1dpnCy0Oa9V02tfNo6KIzp"}, 
        {"genre": "eerie", "trackId" : "1dpnCy0Oa9V02tfNo6KIzp"},
@@ -20,13 +23,13 @@ dataset = [{"genre": "eerie", "trackId" : "1dpnCy0Oa9V02tfNo6KIzp"},
        {"genre": "eerie", "trackId" : "1dpnCy0Oa9V02tfNo6KIzp"},
        {"genre": "eerie", "trackId" : "1dpnCy0Oa9V02tfNo6KIzp"}]
 
-# return data as [genre, track_name, track_id, acousticness, danceability, energy, instrumentalness,
+# return data as [genre, track_name, artist, track_id, acousticness, danceability, energy, instrumentalness,
 # key, liveness, loudness, mode, speechiness, tempo, time_signature, valence]
 data = [[]]
 for i in dataset:
     genre = i.get("genre")
     trackId = i.get("trackId")
-    data.append([genre, get_track_name(trackId), trackId, 
+    data.append([genre, get_track_name(trackId), get_track_artist(trackId), trackId,
         get_acoustic_feature(trackId, 'acousticness'),
         get_acoustic_feature(trackId, 'danceability'),
         get_acoustic_feature(trackId, 'energy'),
@@ -42,7 +45,7 @@ for i in dataset:
        ])
 
 # create pandas DataFrame
-df = pd.DataFrame(data, columns = ['genre', 'track_name', 'track_id', 'acousticness', 'danceability', 'energy',
+df = pd.DataFrame(data, columns = ['genre', 'track_name', 'artist','track_id', 'acousticness', 'danceability', 'energy',
                                    'instrumentalness', 'key', 'liveness', 'loudness', 'mode', 'speechiness',
                                    'tempo', 'time_signature', 'valence'])
 
